dbb12307fac2536e504a0b3a20c01b0a77473527
AlanCILee <alan.changil.lee@gmail.com>
2016-07-23 04:35:00 +0900

    Fix file copy issue and menu

--- sales/php/updateAndNewPos.php
+++ sales/php/updateAndNewPos.php
@@ -16,7 +16,7 @@
 
 	      if(empty($errors)==true){
 	         move_uploaded_file($_FILES["myfile"]["tmp_name"],
-	                                "sales/" . $_FILES["myfile"]["name"]);
+	                                "../" . $_FILES["myfile"]["name"]);
 	                                
 
 	     echo "<script>document.getElementById('img').innerHTML = 'Upload successful!'</script>";
